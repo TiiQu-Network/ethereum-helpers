18c0d800e932fa4afc24ee7ffbe78a3f4976f7d8
Marcin Wysokiński <marcinwysokinski@Marcins-MacBook-Pro.local>
2018-04-14 18:10:12 +0200

    contract balance

--- client/client.go
+++ client/client.go
@@ -9,12 +9,13 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/Samyoul/go-handle"
+	"github.com/TiiQu-Network/ethereum-helpers/contracts"
 )
 
 type Client struct {
-	errorHandler  *handlers.ErrorHandler
-	EtherClient   *ethclient.Client
-	RpcClient     *rpc.Client
+	errorHandler *handlers.ErrorHandler
+	EtherClient  *ethclient.Client
+	RpcClient    *rpc.Client
 }
 
 func Dial(rawUrl string) (*Client) {
@@ -30,16 +31,17 @@ func Dial(rawUrl string) (*Client) {
 	handler.Handle(err, "Failed to connect to the Ethereum network:")
 
 	return &Client{
-		errorHandler:  handler,
-		EtherClient:   ethClient,
-		RpcClient:     newRpcClient,
+		errorHandler: handler,
+		EtherClient:  ethClient,
+		RpcClient:    newRpcClient,
 	}
 }
 
 // Internal call function
-func (c Client) rpcCall(method string) (string) {
+func (c Client) rpcCall(method string, args ...interface{}) (string) {
 	var output string
-	err := c.RpcClient.Call(&output, method)
+
+	err := c.RpcClient.Call(&output, method, args...)
 	c.errorHandler.Handle(err, "RPC call failed - Method: %s", method)
 
 	return output
@@ -71,6 +73,18 @@ func (c Client) BlockNumber() (*big.Int) {
 	return big.NewInt(blockNumberInt)
 }
 
+// Get transaction count
+func (c Client) TransactionCount(hexAddress string) (*big.Int) {
+	response := c.rpcCall("eth_getTransactionCount", hexAddress)
+
+	// Convert the hex response data into an int
+	count, err := strconv.ParseInt(response, 0, 64)
+	c.errorHandler.Handle(err, "Failed to parse blockNumber from hex to int:")
+
+	// Convert int into big.Int for use with other method calls
+	return big.NewInt(count)
+}
+
 /**
  *  Ether Requests
  */
@@ -82,4 +96,27 @@ func (c Client) BalanceAt(hexAddress string) (*big.Int) {
 	c.errorHandler.Handle(err, "Failed to get balance:")
 
 	return bal
+}
+
+func (c *Client) GetContractBalance(contract string, wallet string) (*big.Int, error) {
+	var err error
+
+	conn := c.EtherClient
+
+	token, err := contracts.NewTokenCaller(common.HexToAddress(contract), conn)
+	if err != nil {
+		return nil, err
+	}
+
+	address := common.HexToAddress(wallet)
+	if err != nil {
+		return nil, err
+	}
+
+	balance, err := token.BalanceOf(nil, address)
+	if err != nil {
+		return nil, err
+	}
+
+	return balance, nil
 }
