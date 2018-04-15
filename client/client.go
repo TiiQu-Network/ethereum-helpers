361a2ae6fe5bc2c1414c37c59c03a7b35ac40e96
Marcin Wysokiński <marcinwysokinski@Marcins-MacBook-Pro.local>
2018-04-15 18:54:47 +0200

    GetContractLogs

--- client/client.go
+++ client/client.go
@@ -10,6 +10,8 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/Samyoul/go-handle"
 	"github.com/TiiQu-Network/ethereum-helpers/contracts"
+	"github.com/ethereum/go-ethereum"
+	"github.com/TiiQu-Network/ethereum-helpers/contracts/events"
 )
 
 type Client struct {
@@ -119,4 +121,47 @@ func (c *Client) GetContractBalance(contract string, wallet string) (*big.Int, e
 	}
 
 	return balance, nil
+}
+
+type ContractLog struct {
+	TxHash  string
+	Removed bool
+	From    string
+	To      string
+	Value   *big.Int
+	Block   uint64
+}
+
+func (c *Client) GetContractLogs(contract string, from *big.Int, to *big.Int) ([]ContractLog, error) {
+	query := ethereum.FilterQuery{
+		FromBlock: from,
+		ToBlock:   to,
+		Topics:    [][]common.Hash{{events.TransferTokenSignature}},
+		Addresses: []common.Address{common.HexToAddress(contract)},
+	}
+
+	results, err := c.EtherClient.FilterLogs(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]ContractLog, len(results))
+
+	for index, element := range results {
+
+		event := new(events.EventTokenTransfer)
+		event.Unmarshal(&element)
+
+		res[index] = ContractLog{
+			TxHash:  element.TxHash.String(),
+			Removed: element.Removed,
+			From:    event.From.String(),
+			To:      event.To.String(),
+			Value:   event.Value,
+			Block: element.BlockNumber,
+		}
+
+	}
+
+	return res, nil
 }
